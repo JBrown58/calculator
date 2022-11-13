4223b820f4c1bce341f752adab654d8c53df7c8a
ForkEyeee <shawncarter123456@gmail.com>
2022-11-13 15:06:21 -0500

    removing script from html

---
+++ scripts.js
@@ -0,0 +1,79 @@
+function add (a, b) {
+    return a + b;
+  }
+  
+  function subtract (a, b) {
+    return a - b;
+  }
+  
+  function multiply (a, b) {
+    return a * b;
+  }
+  
+  function divide (a, b) {
+    return a / b;
+  }
+  
+  function operate (a, b, func) {
+    return func(a, b)
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  /*function sum(...args) {
+      let total = 0;
+          for (let x of args) {
+           total += x;
+    }
+    return total;
+  }
+  
+  
+  function diff(...args) {
+      let total = 0;
+          for (let x of args) {
+           total -= x;
+    }
+    return total;
+  }
+  
+  function prod(...args) {
+      let total = 0;
+          for (let x of args) {
+           total *= x;
+    }
+    return total;
+  }
+  
+  function quo(...args) {
+      let total = args[0]/args[1];
+      args.splice(0, 2);
+  if (args.length > 0) { 
+  for (let i = 0; i = args.length; i++) {
+     total /= args[0];
+     args.splice(0, 1);
+  } 
+     return total;
+  }
+  return total;
+  }*/
