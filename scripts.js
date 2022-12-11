b29a7b84d536425a7076b1308307e37c9ade6707
ForkEyeee <shawncarter123456@gmail.com>
2022-12-10 22:53:12 -0500

    added event listener to buttons to display numbers on screen

--- scripts.js
+++ scripts.js
@@ -1,24 +1,54 @@
 function add (a, b) {
-    return a + b;
-  }
-  
-  function subtract (a, b) {
-    return a - b;
-  }
-  
-  function multiply (a, b) {
-    return a * b;
-  }
-  
-  function divide (a, b) {
-    return a / b;
-  }
-  
-  function operate (a, b, func) {
-    return func(a, b)
-  }
-  
-  
+  return a + b;
+}
+
+function subtract (a, b) {
+  return a - b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function divide (a, b) {
+  return a / b;
+}
+
+function operate (a, b, func) {
+  return func(a, b)
+}
+
+let displayValue = document.getElementById('display')
+
+function displayNumbers () {
+  //when a button is pressed, its value will be stored in calcDisplay
+}
+
+let list = document.getElementsByClassName('button-item');
+
+for(let i=0; i < list.length; i++){
+  list[i].addEventListener("click", function () {
+    displayValue.innerHTML= list[i].innerHTML
+
+  })
+}
+
+function calc () {
+  if (document.getElementById('display') == 2) {
+    console.log('here')
+} else {console.log('nope')}}
+
+
+
+
+
+
+// When 1-9 are pressed, values will show in display
+// when 1-9 buttons are pressed, event listener will store values in valueDisplay. valueDisplay will be shown in "id" div
+
+
+
+
   
   
   
