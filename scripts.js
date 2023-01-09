65f2e07f10286e2a1ceb92286a4d0525ce45e2bd
ForkEyeee <shawncarter123456@gmail.com>
2023-01-09 10:06:53 -0500

    functional calculator, needs floating funcitonality as well as clear/delete

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ function multiply (a, b) {
 }
 
 function divide (a, b) {
-  return b / a;
+  return a / b;
 }
 
 function noOperand (a) {
@@ -23,46 +23,160 @@ function operate (a, b, func) {
 }
 
 let displayValue = document.getElementById('display')
-let displaySummary = document.getElementById('displaySummary')
 let solution = document.getElementById('solution')
-
 let list = document.getElementsByClassName('button-item');
 
-//everytime the button is clicked, it will add that number to the displayValue
 let firstNumber;
 let secondNumber = "";
 let operand;
+let result;
  
 
 for(let i=0; i < list.length; i++){
-  list[i].addEventListener("click", function (event) {
-    if (event.target ===  list[3] ||event.target === list[7] ||event.target === list[11] ||event.target === list[15]) {
-      displaySummary.innerHTML = firstNumber
+ list[i].addEventListener("click", function (event) {
+  if (event.target ===  list[3] ||event.target === list[7] ||event.target === list[11] ||event.target === list[15]) {
+    if (secondNumber === ""){}
+    if (typeof operand !== "undefined") {
       secondNumber = displayValue.innerHTML
-      displayValue.innerHTML = ""
-      operand = event.target.innerHTML
-      
-            
-    } else if (event.target === list[14]) {
-     if (typeof operand === "undefined") {
-      solution.innerHTML = parseInt(firstNumber)
+      displayValue.innerHTML = ''
+      if (operand === "*") {
+        result = operate (parseInt(firstNumber), parseInt(secondNumber), multiply )
+        firstNumber = result
+      } else if (operand === "-") {
+        result = operate (parseInt(firstNumber), parseInt(secondNumber), subtract )
+        firstNumber = result
+      } else if (operand === "+") {
+        result = operate (parseInt(firstNumber), parseInt(secondNumber), add )
+        firstNumber = result
+        } else if (operand === "/") {
+          result = operate (parseInt(firstNumber), parseInt(secondNumber), divide  )
+          firstNumber = result
+        }
+    }
+    if (typeof firstNumber !== "undefined" && secondNumber === ''){
+      secondNumber = displayValue.innerHTML
+      displayValue.innerHTML = ''
+      if (operand === "*") {
+        result = operate (parseInt(firstNumber), parseInt(secondNumber), multiply )
+      } else if (operand === "-") {
+        result = operate (parseInt(firstNumber), parseInt(secondNumber), subtract )
+      } else if (operand === "+") {
+        result = operate (parseInt(firstNumber), parseInt(secondNumber), add )
+        } else if (operand === "/") {
+          result = operate (parseInt(firstNumber), parseInt(secondNumber), divide  )
+        }
+      firstNumber = result
+      secondNumber = ""
       return;
-     } else if (operand === "*") {
-      operand = multiply
-     } else if (operand === "-") {
-      operand = subtract
-     } else if (operand === "+") {
-      operand = add
-     } else if (operand === "/") {
-      operand = divide
-      }
-      solution.innerHTML = operate (parseInt(firstNumber), parseInt(secondNumber), operand )
-    } else {displayValue.innerHTML += event.target.innerHTML}
-    firstNumber = displayValue.innerHTML 
-      }
-      )}
+    }
+    operand = event.target.innerHTML
+    if (typeof firstNumber !== "undefined" && secondNumber !== '' && typeof result !== "undefined"){
+      return;
+    }
+    if(typeof firstNumber !== "undefined") {
+      secondNumber = displayValue.innerHTML
+      if (operand === "*") {
+        operand = multiply
+        } else if (operand === "-") {
+        operand = subtract
+        } else if (operand === "+") {
+          result = operate (parseInt(firstNumber), parseInt(secondNumber), add )
+          firstNumber = result
+        } else if (operand === "/") {
+        operand = divide
+        }
+      return;
+    }
+      firstNumber = displayValue.innerHTML
+    displayValue.innerHTML = ""
+     } else if (event.target === list[14]) {
+      if (typeof operand === "undefined") {
+        result = operate (parseInt(firstNumber), parseInt(secondNumber), operand )
+        displayValue.innerHTML = result
+          return;
+      }  secondNumber = displayValue.innerHTML
+      //result = operate (parseInt(firstNumber), parseInt(secondNumber), operand ) //calcsolution
+      if (operand === "*") {
+        result = operate (parseInt(firstNumber), parseInt(secondNumber), multiply )
+      } else if (operand === "-") {
+        result = operate (parseInt(firstNumber), parseInt(secondNumber), subtract )
+      } else if (operand === "+") {
+        result = operate (parseInt(firstNumber), parseInt(secondNumber), add )
+        } else if (operand === "/") {
+          result = operate (parseInt(firstNumber), parseInt(secondNumber), divide  )
+        }
+     displayValue.innerHTML = result
+    } else {
+      displayValue.innerHTML += event.target.innerHTML
       
-      //function operate (firstNumber, )
+      return;
+      }
+      if(typeof firstNumber !== "undefined") {
+        if (event.target.innerHTML === "=") {
+          firstNumber = result
+          secondNumber = ""
+          operand = ""
+          return;
+        }
+        secondNumber = displayValue.innerHTML
+      }
+      /* if( secondNumber !== '' || typeof firstNumber !== "undefined") {
+          if (operand === "*") {
+          operand = multiply
+          } else if (operand === "-") {
+          operand = subtract
+          } else if (operand === "+") {
+          operand = add
+          } else if (operand === "/") {
+          operand = divide
+          }
+       result = operate (parseInt(firstNumber), parseInt(secondNumber), operand )
+       result = display.innerHTML
+      }
+
+      if(typeof firstNumber !== "undefined" || secondNumber !== ''  ) {
+        if(event.target ===  list[3] ||event.target === list[7] ||event.target === list[11] ||event.target === list[15]) {
+         result = operate (parseInt(firstNumber), parseInt(secondNumber), operand )
+        }
+      }*/
+        } 
+        
+      )
+     
+    }
+      
+      
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //function operate (firstNumber, )
 
 
 
